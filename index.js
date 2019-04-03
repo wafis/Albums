ff0e24820a49ee194f41c0b45c81f8d7dda05420
Wasif Rahman <wrahman2@student.gsu.edu>
2019-04-03 12:57:24 -0400

    Created an app that allowed me to become familliar with react-native during the hackathon

---
+++ index.js
@@ -0,0 +1,18 @@
+// Import a Library to help create a componenet
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a Component
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Header headerText="Albums" />
+      <AlbumList />
+    </View>
+  );
+};
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
